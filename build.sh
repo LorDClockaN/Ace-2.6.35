42869ceaa1456a4b10c0a2b93ea0e63d91826d58
LorD ClockaN <davor@losinj.com>
2011-11-03 19:21:53 +0100

    edit build script for VIVO

--- build.sh
+++ build.sh
@@ -3,17 +3,18 @@
 make -j8 ARCH=arm CROSS_COMPILE=$CCOMPILER EXTRA_AFLAGS='-mfpu=vfpv3 -ftree-vectorize -floop-interchange -floop-strip-mine -floop-block';
 
 
-cp arch/arm/boot/zImage ../finished/sense/;
-cp drivers/net/wireless/bcm4329_204/bcm4329.ko ../finished/sense/;
-cp drivers/net/tun.ko ../finished/sense/;
-cp fs/cifs/cifs.ko ../finished/sense/;
+cp arch/arm/boot/zImage ../finished/vivo/;
+cp drivers/net/wireless/bcm4329_204/bcm4329.ko ../finished/vivo/;
+cp drivers/net/tun.ko ../finished/vivo/;
+cp fs/cifs/cifs.ko ../finished/vivo/;
 #cp drivers/staging/zram/zram.ko ../finished/;
 #cp lib/lzo/lzo_compress.ko ../finished/;
-cp arch/arm/mach-msm/qdsp5v2_1x/qc_pcm_in.ko ../finished/sense/;
-cp drivers/net/kineto_gan.ko ../finished/sense/;
+cp arch/arm/mach-msm/qdsp5v2_1x/qc_pcm_in.ko ../finished/vivo/;
+cp drivers/net/kineto_gan.ko ../finished/vivo/;
+cp fs/nls/nls_utf8.ko ../finished/vivo/;
 
 echo "COMPILING FINISHED!!!";
 read;
-alias make='make -j4 ARCH=arm CROSS_COMPILE=$CCOMPILER'
+alias make='make -j8 ARCH=arm CROSS_COMPILE=$CCOMPILER'
 make clean
 
